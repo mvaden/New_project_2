c6b16cefc22202b8431ecf356be105cee8eaa9c5
Matt <mvaden08@gmail.com>
2016-04-27 16:08:09 -0400

    added link functionality for assignment and student controllers and views; created a vew for assignments.

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -2,4 +2,31 @@ class AssignmentsController < ApplicationController
   def index
     @assignments = Assignment.all
   end
+
+  def show
+    @assignment = Assignment.find(params[:id])
+  end
+
+  def new
+    @assignment = Assignment.new
+  end
+
+  def create
+    @assignment = Assignment.create!(assignment_params)
+    flash[:notice] = "Assignment created successfully!"
+    redirect_to @student
+    if params[:assignment][:name] == ""
+      flash[:alert] = "Assignment can't be blank!"
+      redirect_to @student
+    else
+      @assignment = Assignment.create(assignment_params)
+      redirect_to @assignment
+    end
+  end
+
+  def destroy
+    @assignment = assignment.find(params[:id])
+    @assignment.destroy
+    redirect_to assignments_path
+  end
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,7 +4,30 @@ class StudentsController < ApplicationController
   end
 
   def show
-    @todo = Todo.find(params[:id])
+    @student = Student.find(params[:id])
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Artist.create!(student_params)
+    flash[:notice] = "student created successfully!"
+    redirect_to @student
+    if params[:student][:name] == ""
+      flash[:alert] = "Student can't be blank!"
+      redirect_to @student
+    else
+      @student = Student.create(student_params)
+      redirect_to @student
+    end
+  end
+
+  def destroy
+    @student = student.find(params[:id])
+    @student.destroy
+    redirect_to students_path
   end
 
 end
