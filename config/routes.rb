96b2fed0918589ca88b5506cb50ee0eff1617752
Matt <mvaden08@gmail.com>
2016-04-27 09:08:52 -0400

    Generated controller for assignments, added index for assignments

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'students' => 'students#index'
 
   resources :students, only: [:index, :show]
+  resources :assignments, only: [:index, :show]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
